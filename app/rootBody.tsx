f31fc243f64d0234fda0fc7a0f44bd5f721ea8ad
majidgoblin <majid.hajebi77@gmail.com>
2025-12-19 17:49:48 +0330

    change state managment

--- app/rootBody.tsx
+++ app/rootBody.tsx
@@ -1,14 +1,10 @@
 'use client'
 
-import { Provider as ReduxProvider } from 'react-redux';
-import { store } from './store';
-
-
 export default function RootBody({ children }: { children: React.ReactNode }) {
 
     return (
-        <ReduxProvider store={store}>
+        < >
             {children}
-        </ReduxProvider>
+        </>
     )
 };
